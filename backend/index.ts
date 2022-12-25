ea1ef3fe97952ae911f4fc94c9e3e26f2712ec57
hadialhakeem <hadihakeem2@gmail.com>
2022-12-24 19:55:03 -0500

    create react app

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,12 @@
-import express from 'express';
+import express, {Application, Request, Response} from 'express';
 
-const app = express();
-const port = 3000;
+const app: Application = express();
+const port: number = 3000;
 
-app.get('/', (req, res)=>{
+app.get('/', (req: Request, res: Response)=>{
     res.send('hellow world')
 })
+
 
 app.listen(port, ()=>{
     console.log(`connected succesfully on port ${port}`)
